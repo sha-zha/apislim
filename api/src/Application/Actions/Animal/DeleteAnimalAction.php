75c49d5aa9921ca985c2b1d0cf0a334e9c768fec
Adeline <adeline.servan@gmail.com>
2021-01-14 10:58:30 +0400

    update des animals

---
+++ api/src/Application/Actions/Animal/DeleteAnimalAction.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+                        
+namespace App\Application\Actions\Animal;
+                        
+use Psr\Http\Message\ResponseInterface as Response;
+                        
+class DeleteAnimalAction extends AnimalAction
+{
+    protected function action(): Response
+    {
+        $animalId = (int) $this->resolveArg('id');
+        $animal = $this->animal->destroy($animalId);
+        return $this->respondWithData($animal);
+    }
+}    
